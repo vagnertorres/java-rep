a6b7fe101b6b4d02154480d6ce742e4694369e50
Vagner Torres <esdras.torres.vt@gmail.com>
2017-05-10 19:23:29 -0300

    alterando o laço de repetição

---
+++ src/br/com/fiap/rep/Nome.java
@@ -0,0 +1,30 @@
+package br.com.fiap.rep;
+
+import java.util.Scanner;
+
+public class Nome {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma nome com mais de 3 caracteres");
+		String palavra = scanner.next();
+		
+		int i = 0;
+		
+		while (palavra.length() <= 3) {
+			
+			System.out.println("nome inválido, digite novamente");
+			palavra = scanner.next();
+			
+			i++;
+		}
+		
+		scanner.close();
+		
+		if (palavra.length() > 3) {
+			System.out.println("nome validado");
+		}	
+	}
+}
