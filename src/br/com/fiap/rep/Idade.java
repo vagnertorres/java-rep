6807fbacb8bc6a384c3a6198aeeef0ebd12f46c2
Vagner Torres <esdras.torres.vt@gmail.com>
2017-05-15 22:40:36 -0300

    adcionando novo exercício

---
+++ src/br/com/fiap/rep/Idade.java
@@ -0,0 +1,26 @@
+package br.com.fiap.rep;
+
+import java.util.Scanner;
+
+public class Idade {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma idade");
+		int idade = scanner.nextInt();
+		
+		while (idade <= 0 || idade > 120) {
+			
+			System.out.println("idade inválida, digite novamente");
+			idade = scanner.nextInt();
+		}
+		
+		System.out.println("Idade válida");
+		
+		scanner.close();
+
+	}
+
+}
