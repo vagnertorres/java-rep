d2b7231cde6c652acce82efdc64cfacd8d7b6540
Vagner Torres <esdras.torres.vt@gmail.com>
2017-05-16 22:56:44 -0300

    adcionando novo exercício

---
+++ src/br/com/fiap/rep/Genero.java
@@ -0,0 +1,24 @@
+package br.com.fiap.rep;
+
+import java.util.Scanner;
+
+public class Genero {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite m ou f para especificar o seu sexo");
+		String sexo = scanner.next();
+		
+		while (!sexo.equalsIgnoreCase("m") && !sexo.equalsIgnoreCase("f")) {
+			
+			System.out.println("Sexo inválido, digita novamente");
+			sexo = scanner.next();
+		}
+		
+		System.out.println("sexo validado");
+		
+		scanner.close();
+	}
+}
