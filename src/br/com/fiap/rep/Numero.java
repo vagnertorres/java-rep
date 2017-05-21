8421d8a3c0137f3b6d0c3c487c27c2f6993fba31
Vagner Torres <esdras.torres.vt@gmail.com>
2017-05-20 23:36:42 -0300

    adcionando novo exercicio

---
+++ src/br/com/fiap/rep/Numero.java
@@ -0,0 +1,15 @@
+package br.com.fiap.rep;
+
+public class Numero {
+
+	public static void main(String[] args) {
+		
+		int numero = 20;
+		int i = 0;
+		
+		while (i <= numero) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
